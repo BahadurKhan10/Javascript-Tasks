cd5ee03949ce183e90b3bde86d5203cd3e9eee68
Bahadur Khan Swati <57499169+BahadurKhan10@users.noreply.github.com>
2023-04-07 18:33:34 +0500

    Update tasks.js

--- tasks.js
+++ tasks.js
@@ -1,4 +1,4 @@
-// Github link for Typescript of same Tasks is: 
+// Github link for Typescript of same Tasks is: https://github.com/BahadurKhan10/Typescript-Tasks.git
 // Changed logics and make them more accurate, handled null and undefined in prompts etc
 // and made it more user friendly in typescript
 
